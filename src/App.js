5c8a1fc99cdaf4175d63d67853f5c14ee69f5d62
Paul Binns <paul.binns@team.telstra.com>
2018-07-23 07:18:38 +1000

    Setup basic form

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">
-        Hello
-      </h1>
-    </header>
-    <p className="App-intro">
-      Hello
-    </p>
-  </div>
-);
+  <div className="container">
+    <div className="row">
+      <div className="col">
+        <div className="text-center">
+          <form className="form-signin">
+            <h1 className="h3 mb-3 font-weight-normal">
+              Find an event in your area
+            </h1>
+            <label htmlFor="postCode" className="sr-only" id="postCode">
+              Postcode
+            </label>
+            <input
+              name="email"
+              type="email"
+              id="postCode"
+              className="form-control"
+              placeholder="Postcode"
+              required=""
+            />
+            <br />
+            <button className="btn btn-lg btn-primary btn-block" type="submit">
+              Find event
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>);
 
 export default App;
