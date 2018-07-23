977bb9c0b6346990ad77a5b6483c26c2fa1da62c
Paul Binns <paul.binns@team.telstra.com>
2018-07-24 06:56:46 +1000

    Update API function to iterate through supplied values

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,12 @@ import axios from 'axios';
 
 export const baseUrl = 'https://app.ticketmaster.com/discovery/v2/events.json';
 const apiKey = 'Tr7s2LYmcZiFHTL2FXuz2uTjA8XfzyEn';
-const apiGet = postcode => axios.get(`${baseUrl}?apikey=${apiKey}&postalCode=${postcode}`)
-  .then(res => res);
+
+const apiGet = (fields) => {
+  const convertFieldsToArray = Object.entries(fields);
+  const queryStrings = convertFieldsToArray.map(field => `&${field[0]}=${field[1]}`).join();
+  return axios.get(`${baseUrl}?apikey=${apiKey}${queryStrings}`)
+    .then(res => res);
+};
 
 export default apiGet;
