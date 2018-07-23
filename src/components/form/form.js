19bd87feb29d9ecd63f308338add04fd23c8ce56
Paul Binns <paul.binns@team.telstra.com>
2018-07-24 06:56:22 +1000

    Update form logic to handle events from child
    Add custom validation where required

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,73 +1,93 @@
 import React, { Component } from 'react';
 import apiGet from '../../api/api';
 import Events from '../events';
+import FormGroup from './form-group';
 
 class TicketMasterForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      events: [],
-      formInputIsValid: true,
+      displayedEvents: [],
+      fields: {},
+      validFields: {},
     };
   }
 
-  getEvents(postcode) {
-    apiGet(postcode).then((res) => {
+  getEvents(fields) {
+    apiGet(fields).then((res) => {
       const { data: { _embedded: events } } = res;
       this.setState({
-        events: events.events,
+        displayedEvents: events.events,
       });
     });
   }
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const postcode = this.postCodeNode.value;
-    this.setState({ formInputIsValid: true, events: [] });
-    if (!Number.isNaN(Number.parseInt(postcode, 10)) && postcode.length === 4) {
-      this.getEvents(postcode);
-    } else {
-      this.setState({
-        formInputIsValid: false,
-      });
+  validator = (field, val) => {
+    switch (field) {
+      case 'postalCode':
+        return !Number.isNaN(Number.parseInt(val, 10)) && val.length === 4;
+      default:
+        return true;
     }
   };
 
+  validateField = (field, value) => {
+    if (this.validator(field, value)) {
+      this.setState({
+        validFields: { [field]: true },
+        fields: { [field]: value },
+      });
+      return true;
+    }
+    this.setState({
+      validFields: { [field]: false },
+      fields: { [field]: null },
+    });
+    return false;
+  };
+
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { validFields, fields } = this.state;
+    const checkForInvalidFields = Object.values(validFields).some(field => field === false);
+    if (!checkForInvalidFields) {
+      this.setState({ displayedEvents: [] });
+      this.getEvents(fields);
+    }
+  };
+
+  handleOnChange = (field, value) => {
+    this.validateField(field, value);
+  };
+
   render() {
-    const { events = [], formInputIsValid } = this.state;
+    const { displayedEvents = [], validFields } = this.state;
     return (
       <div className="text-center">
         <form className="form-signin" onSubmit={this.handleSubmit}>
           <h1 className="h3 mb-3 font-weight-normal">
             Find an event in your area
           </h1>
-          <div className="form-group">
-            <label htmlFor="postCode" className="sr-only" id="postCode">
-              Postcode
-            </label>
-            <input
-              name="postcode"
-              type="text"
-              id="postCode"
-              className="form-control"
-              placeholder="Postcode"
-              required=""
-              ref={(node) => {
-                this.postCodeNode = node;
-              }}
-            />
-            {!formInputIsValid && (
-              <small className="form-text text-danger text-left">
-                Please enter a 4 digit postcode
-              </small>
-            )}
-          </div>
+          <FormGroup
+            field="postalCode"
+            placeholder="Postcode"
+            validator={validFields}
+            onChangeFunction={this.handleOnChange}
+            validationText="Please enter a 4 digit postcode"
+          />
+          <FormGroup
+            field="keyword"
+            placeholder="Genre or type"
+            validator={validFields}
+            onChangeFunction={this.handleOnChange}
+          />
           <button className="btn btn-lg btn-primary btn-block" type="submit">
             Find event
           </button>
         </form>
-        {events.length > 0
-        && <Events events={events} />
+        {displayedEvents.length > 0
+        && <Events events={displayedEvents} />
         }
       </div>
     );
