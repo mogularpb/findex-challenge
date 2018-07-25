c21c0dc96451be08a4f8cfbb01af71dc521d7854
Paul Binns <paul.binns@team.telstra.com>
2018-07-26 06:54:07 +1000

    Refactor api for better re-usability

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
 
-export const baseUrl = 'https://app.ticketmaster.com/discovery/v2/events.json';
+export const baseUrl = 'https://app.ticketmaster.com/discovery/v2';
 const apiKey = 'Tr7s2LYmcZiFHTL2FXuz2uTjA8XfzyEn';
+const apiKeyQueryParam = `?apikey=${apiKey}`;
 
-const apiGet = (fields) => {
-  const convertFieldsToArray = Object.entries(fields);
-  const queryStrings = convertFieldsToArray.map(field => `&${field[0]}=${field[1]}`).join();
-  return axios.get(`${baseUrl}?apikey=${apiKey}${queryStrings}`)
-    .then(res => res);
-};
+const buildParamQueryString = params => (
+  params
+    ? Object.entries(params)
+      .map(([key, value]) => `&${key}=${value}`)
+      .join()
+    : ''
+);
+const buildApiCall = resource => params => axios.get(`${baseUrl}/${resource}.json${apiKeyQueryParam}${buildParamQueryString(params)}`);
 
-export default apiGet;
+export const getEvents = params => buildApiCall('events')(params);
+export const getGenres = () => buildApiCall('classifications')();
